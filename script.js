83a6a02a78d074fa54a1a957ca05b352021f26f3
Sömen <somendaniel7@gmail.com>
2022-03-09 11:39:36 +0100

    wending machine basic layout

---
+++ script.js
@@ -0,0 +1,9 @@
+let cancel = document
+  .querySelector(".cancel")
+  .addEventListener("click", functi);
+
+function functi() {
+  console.log("asd");
+}
+
+// cancel.addEventListener("click", functi);
